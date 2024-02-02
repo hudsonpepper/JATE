7779f440d5451c04bc8a9a730ff82f127175a480
hudsonpepper <75504341+hudsonpepper@users.noreply.github.com>
2024-02-02 09:55:56 -0500

    editor now stores text

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,48 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('putDb Activated');
+  console.log(content)
+  const jateDB = await openDB('jate', 1);
+  const tx = jateDB.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({id: 1, content})
+  const result = await request;
+  console.log('Put Result: ', result)
+  return result;
+}
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => { 
+  console.log('GET from the database');
+
+  // Create a connection to the database database and version we want to use.
+  const jateDB = await openDB('jate', 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = jateDB.transaction('jate', 'readonly');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+  // Use the .get() method to get all data in the database.
+  const request = store.getAll();
+
+
+  
+  // Get confirmation of the request.
+  const result = await request;
+  let processedResult;
+  if(result.length = 0){
+    processedResult = null
+  }
+  else{
+    processedResult = result[0]
+  }
+  console.log('result.value', processedResult);
+  return processedResult;
+};
 
 initdb();
